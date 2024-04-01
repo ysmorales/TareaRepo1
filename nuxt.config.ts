d7150a1895e92c2112ac4c9ff7c66a7899637af5
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-01 01:04:27 -0400

    Remove 'nuxt-auth-sanctum' dependency
    This commit removes the 'nuxt-auth-sanctum' from the list of devDependencies in package.json. Correlated changes were also made to the pnpm-lock.yaml, dropping the package from the lock file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,4 @@ export default defineNuxtConfig({
         },
     },
     css: ['~/assets/css/main.css'],
-    modules: ['nuxt-auth-sanctum'],
-    sanctum: {
-        baseUrl: 'http://localhost:80', // Laravel API
-        origin: 'http://localhost:3000', // Nuxt app, by default will be used 'useRequestURL().origin'
-    }
 })
