e87660e865c80c45c7f4024137800aee8e2d6569
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-02 16:00:38 -0400

    Add options to NavBar select menu
    The NavBar component's select menu has been expanded to include predefined options for project selection. This enhancement provides a more user-friendly and efficient way for users to choose a specific project from the NavBar.

--- components/builder-components/NavBar/NavBar.vue
+++ components/builder-components/NavBar/NavBar.vue
@@ -6,7 +6,10 @@ import {DsAvatar, DsSelect, DsTypography} from "~/components/DesignSystem";
 <template>
   <div class="flex justify-end border-b border-gray-300 h-[67px] p-3">
     <div class="flex  items-center space-x-2">
-      <DsSelect label="Proyecto" label-style="m-0" size="small"/>
+      <DsSelect
+          :option='[{value:1, text:"Intranet"}, {value:2, text:"Conectados"}, {value:3, text:"Aplicaciones"},{value:4, text:"Personas"},{value:5, text:"SACH"}]'
+          label="Proyecto" label-style="m-0"
+          size="small"/>
       <div class="flex justify-end ml-5 items-center w-[200px]">
         <DsTypography class="m-0 mr-1 " variant="p">Constancia Antonia</DsTypography>
         <DsAvatar size="small"/>
