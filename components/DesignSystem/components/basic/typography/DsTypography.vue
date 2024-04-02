68603beaab995c2e1c9255bbb36121efc6ae3e95
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-02 15:52:21 -0400

    Update NavBar layout and fix typography component
    More details have been added to the NavBar, including a typography and avatar component for display of user information. The DsTypography component has also undergone a slight change in the class filter

--- components/DesignSystem/components/basic/typography/DsTypography.vue
+++ components/DesignSystem/components/basic/typography/DsTypography.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
-import { elements, otherStyle } from "./data";
+import {elements, otherStyle} from "./data";
 import type {
   DsElementNameType,
   IDsTypographyElement,
 } from "../../basic/typography/interfaces";
-import { filterClass } from "../../../utils/filterClass";
-import { predefinedClasses } from "../../../common/propsStyle";
+import {filterClass} from "../../../utils/filterClass";
+import {predefinedClasses} from "../../../common/propsStyle";
 import generateUniqueId from "../../../utils/generateUniqueId";
-import type { ILinkVariant } from "../../navigation/link/interface";
-import { variantTypographyClasses } from "../../navigation/link/library";
-import { computed, ref, onMounted } from "vue";
+import type {ILinkVariant} from "../../navigation/link/interface";
+import {variantTypographyClasses} from "../../navigation/link/library";
+import {computed, ref, onMounted} from "vue";
 
 const props = defineProps({
   text: {
@@ -52,15 +52,16 @@ const element = computed((): IDsTypographyElement => {
 });
 
 const filterClassComp = computed(() => {
-  return filterClass(predefinedClasses, props.class, otherStyle);
+  // return filterClass(predefinedClasses, props.class, otherStyle);
+  return props.class
 });
 </script>
 
 <template>
   <component
-    :is="element.component"
-    :id="id ?? uniqueID"
-    :class="[filterClassComp, variantTypographyClasses[color]]"
+      :is="element.component"
+      :id="id ?? uniqueID"
+      :class="[filterClassComp, variantTypographyClasses[color]]"
   >
     <slot>
       {{ text }}
--- components/builder-components/NavBar/NavBar.vue
+++ components/builder-components/NavBar/NavBar.vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
 
-import {DsSelect} from "~/components/DesignSystem";
+import {DsAvatar, DsSelect, DsTypography} from "~/components/DesignSystem";
 </script>
 
 <template>
   <div class="flex justify-end border-b border-gray-300 h-[67px] p-3">
-    <div class="flex items-center space-x-2">
+    <div class="flex  items-center space-x-2">
       <DsSelect label="Proyecto" label-style="m-0" size="small"/>
+      <div class="flex justify-end ml-5 items-center w-[200px]">
+        <DsTypography class="m-0 mr-1 " variant="p">Constancia Antonia</DsTypography>
+        <DsAvatar size="small"/>
+      </div>
     </div>
   </div>
 </template>
