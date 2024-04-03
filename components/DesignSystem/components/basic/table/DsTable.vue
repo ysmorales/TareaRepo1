e524bb0c146a283fc65559361d53279186d4bf25
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-02 22:14:35 -0400

    Refactor data structure and adjust table styling
    Improved the structure of the data array passed to the DsTable component in pages/index.vue, making it easier to understand and manipulate. Additionally, a minor adjustment was made to the styling of the DsTable component to better handle scenarios when the data array is empty, preventing table distortions that previously occurred.

--- components/DesignSystem/components/basic/table/DsTable.vue
+++ components/DesignSystem/components/basic/table/DsTable.vue
@@ -201,10 +201,10 @@ const hasFilter = computed(() => props.columns.some((column) => column.filter));
     </tr>
     </thead>
     <tbody class="divide-y divide-gray-300 relative">
-    <tr v-if="data.length>0">
+    <tr v-if="!(data.length>0)">
       <td
           :colspan="columns.length"
-          class="px-2 py-2 text-sm font-medium text-gray-900"
+          class=" px-2 py-2 text-sm font-medium text-gray-900 w-full"
       >
         <DsEmptyComponent :loading="loading" @reload="handleReload"/>
       </td>
