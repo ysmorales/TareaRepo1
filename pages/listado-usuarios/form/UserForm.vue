f847ecb4772cc61d59eb92f97446077d96b5bce8
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-04 21:43:39 -0400

    Add UserForm and handle form view transitions
    Added a new UserForm.vue file and embedded it into the Listado-usuarios/index.vue file. Implemented add, cancel, and form/table view transition functionalities. These enhance the UX flow for adding new users in the application.

---
+++ pages/listado-usuarios/form/UserForm.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+
+import {DsButton, DsInput, DsSelect,} from "~/components/DesignSystem";
+
+const emit = defineEmits(['cancelar'])
+
+function handleClick() {
+  emit('cancelar')
+}
+
+
+</script>
+
+<template>
+  <div class="flex justify-end">
+    <DsButton class="m-1" color="tertiary" @click="handleClick">Cancelar</DsButton>
+    <DsButton class="m-1" color="success">Guardar</DsButton>
+  </div>
+  <DsInput label="Nombre" size="full"/>
+  <DsInput label="Correo electrónico" size="full"/>
+  <DsSelect
+      :option="[{value: 1,text:'Editor'},{value: 2,text:'Gestor de noticias'},{value: 3,text:'Administrador'}]"
+      class="mb-4"
+      label="Rol"/>
+  <DsSelect :option="[{value: 1,text:'Sí'},{value: 2,text:'No'}]" class="mb-4" label="Activo"/>
+  <div class="flex justify-end mt-5">
+    <DsButton class="m-1" color="tertiary" @click="handleClick">Cancelar</DsButton>
+    <DsButton class="m-1" color="success">Guardar</DsButton>
+  </div>
+</template>
+
--- pages/listado-usuarios/index.vue
+++ pages/listado-usuarios/index.vue
@@ -3,11 +3,23 @@
 import {columns} from "./columns";
 import {data} from "./data";
 import DsTable from "~/components/DesignSystem/components/basic/table/DsTable.vue";
+import UserForm from "~/pages/listado-usuarios/form/UserForm.vue";
+
+const view = ref<'table' | 'form'>('table')
+
+function handleAdd() {
+  view.value = 'form'
+}
+
+function handleCancelForm() {
+  view.value = 'table'
+}
 </script>
 
 <template>
-  <h1>listado-usuarios</h1>
+  <h1>{{ view == 'table' ? 'Listado usuarios' : 'Nuevo usuario' }}</h1>
   <DsTable
+      v-if="view=='table'"
       :columns="columns"
       :data="data"
       :pagination="{
@@ -15,7 +27,11 @@ import DsTable from "~/components/DesignSystem/components/basic/table/DsTable.vu
           currentPage: 1,
           itemsPerPage: 5,
           itemsPerPageOptions: [5, 10, 50],
-          }" checkboxSelection sort striped/>
+          }"
+      add-button-label="Nuevo usuario" checkboxSelection sort striped @add-row="handleAdd"/>
+  <div v-if="view=='form'">
+    <UserForm @cancelar="handleCancelForm"/>
+  </div>
 </template>
 
 <style scoped>
