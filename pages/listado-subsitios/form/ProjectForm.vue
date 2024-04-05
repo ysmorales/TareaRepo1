fd0266cabbd7138a23a5401bce3126da7831a3d4
Jorge Maikel Perez Guedes <jmperezg.srv@chileatiende.cl>
2024-04-04 21:18:05 -0400

    Refactor ProjectForm component imports and structure
    The commit trims down the DesignSystem component imports in ProjectForm.vue and extends the form's structure. The changes include additional DsInput and DsSelect components, expanding options for category selection and adding a text input for S3 file associations.

--- pages/listado-subsitios/form/ProjectForm.vue
+++ pages/listado-subsitios/form/ProjectForm.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 
-import {DsButton, DsCheck, DsFile, DsInput, DsSelect, DsTabItem, DsTabs} from "~/components/DesignSystem";
+import {DsButton, DsInput, DsSelect,} from "~/components/DesignSystem";
 
 const emit = defineEmits(['cancelar'])
 
@@ -22,6 +22,20 @@ function handleClick() {
       :option="[{value:1,text:'Trámite en línea'},{value:1,text:'Trámite presencial'},{value:1,text:'Trámite por video atención'},
       {value:1,text:'Beneficios y pensiones'},{value:1,text:'Trámites y servicios'},{value:1,text:'Unidos'},
       {value:1,text:'Gestor de documentos'},{value:1,text:'Galería de imágenes'},{value:1,text:'Páginas'}]"
+      class="mb-3"
+      label="Categoría raíz de páginas asociadas"/>
+  <DsInput label="Carpeta raíz de archivos S3 asociadas" size="full" type="text"/>
+  <DsSelect
+      :option="[{value:1,text:'Trámite en línea'},{value:1,text:'Trámite presencial'},{value:1,text:'Trámite por video atención'},
+      {value:1,text:'Beneficios y pensiones'},{value:1,text:'Trámites y servicios'},{value:1,text:'Unidos'},
+      {value:1,text:'Gestor de documentos'},{value:1,text:'Galería de imágenes'},{value:1,text:'Páginas'}]"
+      class="mb-3"
+      label="Categoría raíz de páginas asociadas"/>
+  <DsSelect
+      :option="[{value:1,text:'Trámite en línea'},{value:1,text:'Trámite presencial'},{value:1,text:'Trámite por video atención'},
+      {value:1,text:'Beneficios y pensiones'},{value:1,text:'Trámites y servicios'},{value:1,text:'Unidos'},
+      {value:1,text:'Gestor de documentos'},{value:1,text:'Galería de imágenes'},{value:1,text:'Páginas'}]"
+      class="mb-3"
       label="Categoría raíz de páginas asociadas"/>
   <div class="flex justify-end mt-5">
     <DsButton class="m-1" color="tertiary" @click="handleClick">Cancelar</DsButton>
