28894cdcb05fd97ac3ec8fb2136eb9bd431d8221
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-11 17:32:30 -0400

    Add DsSelect component to default layout
    The commit introduces the DsSelect component to the default layout, specifically in the 'final-navBar' template slot. This provides users with a new selection feature with predefined options in the navigation bar.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
 import VerticalMenu from "~/components/builder-components/verticalMenu/VerticalMenu.vue";
 import NavBar from "~/components/builder-components/NavBar/NavBar.vue";
 import DsUnidosLayout from "~/components/DesignSystem/components/layout/unidos-layout/DsUnidosLayout.vue";
+import {DsSelect} from "~/components/DesignSystem";
 </script>
 
 <template>
@@ -20,6 +21,12 @@ import DsUnidosLayout from "~/components/DesignSystem/components/layout/unidos-l
     </div>
     <div class="mt-56">
         <DsUnidosLayout logo="images/builder/creador-logotipo.jpg">
+            <template v-slot:final-navBar>
+                <DsSelect
+                    :option='[{value:"1", text:"Intranet"}, {value:"2", text:"Conectados"}, {value:"3", text:"Aplicaciones"},{value:"4", text:"Personas"},{value:"5", text:"SACH"}]'
+                    label="Proyecto" label-style="m-0"
+                    size="small"/>
+            </template>
             <div class="container">
                 <slot/>
             </div>
