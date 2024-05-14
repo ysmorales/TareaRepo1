2a770b53674b515a8ccf9d51e7ec137a148146dd
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-14 17:49:59 -0400

    Update DraggableArea to show relevant messages
    Updated DraggableArea.vue to show relevant messages only when there are no items in the component. Furthermore, changes have been made to defineProps, moving from a default string array to an array defined by the component properties. The provided drop logic has been commented out and replaced with an alert for testing purposes.

--- components/builder-components/dragable-area/DraggableArea.vue
+++ components/builder-components/dragable-area/DraggableArea.vue
@@ -2,7 +2,14 @@
 import {ref, reactive} from 'vue'
 import {DsIcon} from "~/components/DesignSystem";
 
-const items = ref<string[]>(['Item 1', 'Item 2', 'Item 3'])
+defineProps({
+    items: {
+        type: Array,
+        default: () => []
+    }
+
+})
+
 let draggedItem: any = reactive({})
 
 const dragStart = (event: DragEvent, item: string) => {
@@ -10,11 +17,12 @@ const dragStart = (event: DragEvent, item: string) => {
 }
 
 const drop = (event: DragEvent) => {
-    const index = items.value.indexOf(draggedItem)
-    if (index > -1) {
-        items.value.splice(index, 1)
-    }
-    items.value.push(draggedItem)
+    alert('dropped')
+    // const index = items.value.indexOf(draggedItem)
+    // if (index > -1) {
+    //     items.value.splice(index, 1)
+    // }
+    // items.value.push(draggedItem)
 }
 </script>
 
@@ -24,9 +32,11 @@ const drop = (event: DragEvent) => {
         @drop="drop($event)"
         @dragover.prevent
     >
-        <DsIcon class="mb-4" name="hand-pointer" size="medium"/>
-        <p class="mb-2">Drag elements here</p>
-        <p>Grab an element from the left and drop it here</p>
+        <div v-if="items.length==0" class="flex flex-col justify-center items-center">
+            <DsIcon class="mb-4" name="hand-pointer" size="medium"/>
+            <p class="mb-2">Drag elements here</p>
+            <p>Grab an element from the left and drop it here</p>
+        </div>
         <div
             v-for="(item, index) in items"
             :key="index"
