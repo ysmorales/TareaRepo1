f53bc3f46f9d6136c2c58719258bdccd5cee4bb1
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-14 19:45:12 -0400

    Update DsTextArea component reformatting and layout modifications
    Significant reformatting occurred in "DsTextArea.vue" for improved readability and coding standards compliance. This includes a default size value change from "normal" to "full" for text area components. Also, the layout for label, textarea, and error/help message display has been revamped for a better structure.

--- components/DesignSystem/components/form/textArea/DsTextArea.vue
+++ components/DesignSystem/components/form/textArea/DsTextArea.vue
@@ -1,79 +1,79 @@
 <script lang="ts" setup>
 import {
-  predefinedClasses,
-  sizeTextAreaClasses,
+    predefinedClasses,
+    sizeTextAreaClasses,
 } from "../../../common/propsStyle";
-import type { ISizeText } from "../../../interfaces/elements";
-import { filterClass } from "../../../utils/filterClass";
+import type {ISizeText} from "../../../interfaces/elements";
+import {filterClass} from "../../../utils/filterClass";
 import generateUniqueId from "../../../utils/generateUniqueId";
 import useFocus from "../../../composables/useFocus";
-import { translateError } from "../../../utils/translateErrorMessage";
+import {translateError} from "../../../utils/translateErrorMessage";
 import buildAriaLabels from "../../../utils/buildAriaLabels";
-import { computed } from "vue";
-import type{  PropType } from "vue";
+import {computed} from "vue";
+import type {PropType} from "vue";
 
 const props = defineProps({
-  modelValue: {
-    type: String,
-    default: "",
-  },
+    modelValue: {
+        type: String,
+        default: "",
+    },
 
-  error: {
-    type: String,
-    default: null,
-  },
+    error: {
+        type: String,
+        default: null,
+    },
 
-  label: {
-    type: String,
-    default: "Default label",
-  },
-  id: {
-    type: String,
-  },
+    label: {
+        type: String,
+        default: "Default label",
+    },
+    id: {
+        type: String,
+    },
 
-  size: {
-    type: String as () => ISizeText,
-    default: "normal",
-  },
+    size: {
+        type: String as () => ISizeText,
+        default: "full",
+    },
 
-  radius: {
-    type: Boolean,
-    default: false,
-  },
+    radius: {
+        type: Boolean,
+        default: false,
+    },
 
-  disabled: {
-    type: Boolean,
-    default: false,
-  },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
 
-  readOnly: {
-    type: Boolean,
-    default: false,
-  },
+    readOnly: {
+        type: Boolean,
+        default: false,
+    },
 
-  required: {
-    type: Boolean,
-    default: false,
-  },
+    required: {
+        type: Boolean,
+        default: false,
+    },
 
-  placeholder: {
-    type: String as PropType<string | null>,
-  },
+    placeholder: {
+        type: String as PropType<string | null>,
+    },
 
-  focus: {
-    type: Boolean,
-    default: false,
-  },
+    focus: {
+        type: Boolean,
+        default: false,
+    },
 
-  helpMessage: {
-    type: String as PropType<string | null>,
-    default: null,
-  },
+    helpMessage: {
+        type: String as PropType<string | null>,
+        default: null,
+    },
 
-  class: {
-    type: String,
-    default: "",
-  },
+    class: {
+        type: String,
+        default: "",
+    },
 });
 
 const inputId = computed(() => generateUniqueId("textarea"));
@@ -81,50 +81,52 @@ const labelId = computed(() => `${inputId.value}-label`);
 const errorMessageId = computed(() => `${inputId.value}-error-message`);
 const helpMessageId = computed(() => `${inputId.value}-help-message`);
 const elementSizeComputed = computed(() => {
-  const size = props.size || "normal"; // Si props.size es undefined, usamos 'normal'
-  return sizeTextAreaClasses[size];
+    const size = props.size || "full"; // Si props.size es undefined, usamos 'normal'
+    return sizeTextAreaClasses[size];
 });
 const emit = defineEmits(["update:modelValue"]);
 
 const filterClassComp = computed(() => {
-  return filterClass(predefinedClasses, props.class);
+    // return filterClass(predefinedClasses, props.class);
+    return props.class
 });
 
-const { elementRef: textAreaRef } = useFocus(
-  () => props.focus,
-  () => props.error,
+const {elementRef: textAreaRef} = useFocus(
+    () => props.focus,
+    () => props.error,
 );
 
 function handleInput(e: Event) {
-  const target = e.target as HTMLInputElement;
-  emit("update:modelValue", target.value);
+    const target = e.target as HTMLInputElement;
+    emit("update:modelValue", target.value);
 }
 
 const hasError = computed(() => !!props.error);
 const errorMessage = computed(() => translateError(props.error));
 
 const ariaLabels = computed(() =>
-  buildAriaLabels(props, {
-    label: labelId.value,
-    error: errorMessageId.value,
-    helpMessage: helpMessageId.value,
-  }),
+    buildAriaLabels(props, {
+        label: labelId.value,
+        error: errorMessageId.value,
+        helpMessage: helpMessageId.value,
+    }),
 );
 </script>
 
 <template>
-  <label v-if="label" :id="labelId" :for="inputId" class="mb-2">
-    {{ label }}
-    <span v-if="required" aria-hidden="true" class="required-marker">*</span>
-  </label>
+    <div class="w-full">
+        <label v-if="label" :id="labelId" :for="inputId">
+            {{ label }}
+            <span v-if="required" aria-hidden="true" class="required-marker">*</span>
+        </label>
 
-  <textarea
-    :id="id ?? inputId"
-    ref="textAreaRef"
-    :aria-invalid="hasError"
-    :aria-labelledby="ariaLabels"
-    :aria-required="required"
-    :class="[
+        <textarea
+            :id="id ?? inputId"
+            ref="textAreaRef"
+            :aria-invalid="hasError"
+            :aria-labelledby="ariaLabels"
+            :aria-required="required"
+            :class="[
       { error: hasError },
       filterClassComp,
       'border',
@@ -132,18 +134,19 @@ const ariaLabels = computed(() =>
       { rounded: radius },
       elementSizeComputed,
     ]"
-    :disabled="disabled"
-    :placeholder="placeholder ?? 'Textarea'"
-    :readonly="readOnly"
-    :value="modelValue"
-    @input="handleInput"
-  />
+            :disabled="disabled"
+            :placeholder="placeholder ?? 'Textarea'"
+            :readonly="readOnly"
+            :value="modelValue"
+            @input="handleInput"
+        />
 
-  <label v-if="hasError" :id="errorMessageId" class="error-message block mb-0">
-    {{ errorMessage }}
-  </label>
+        <label v-if="hasError" :id="errorMessageId" class="error-message block mb-0">
+            {{ errorMessage }}
+        </label>
 
-  <label v-if="helpMessage" :id="helpMessageId" class="help-message block">
-    {{ helpMessage }}
-  </label>
+        <label v-if="helpMessage" :id="helpMessageId" class="help-message block">
+            {{ helpMessage }}
+        </label>
+    </div>
 </template>
