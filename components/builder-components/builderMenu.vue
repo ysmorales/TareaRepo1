87e1f0e798bacb44a75ca8f518039181e10cd8f7
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-14 13:08:26 -0400

    Removed NavBar component and added builderMenu component
    Removed the NavBar component from the builder-components directory. Added a new component called builderMenu in the same directory. Updated the builderStore by adding a new ref for sideMenuType and a function to change it.

---
+++ components/builder-components/builderMenu.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+import {DsButton} from "~/components/DesignSystem";
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <DsButton>DsInput</DsButton>
+        <DsButton>DsSelect</DsButton>
+        <DsButton>DsButton</DsButton>
+    </div>
+</template>
--- stores/builderStore.ts
+++ stores/builderStore.ts
@@ -1,6 +1,7 @@
 export const useCounterStore = defineStore('counter', () => {
     const count = ref(0)
     const name = ref('Eduardo')
+    const sideMenuType = ref<'default' | 'builder'>('default')
     const doubleCount = computed(() => count.value * 2)
 
     function increment() {
@@ -11,5 +12,9 @@ export const useCounterStore = defineStore('counter', () => {
         name.value = newName;
     }
 
-    return {count, name, doubleCount, increment, changeName}
+    function changeSideMenuType(newName: 'default' | 'builder') {
+        sideMenuType.value = newName;
+    }
+
+    return {count, name, doubleCount, increment, changeName, sideMenuType, changeSideMenuType}
 })
