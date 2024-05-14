e445ef4a2eed730ff85f96c0ad011dbde8259204
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-14 18:10:19 -0400

    Add IItemBuilder interface and update builderStore
    A new IItemBuilder interface is added for better type checking. Also, the builderStore is updated to import this new interface and introduces an additional function to change the currentDragItem. The currentDragItem represents the item currently being dragged in the builder view.

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,4 @@
+export interface IItemBuilder {
+    type: string,
+    name: string
+}
--- stores/builderStore.ts
+++ stores/builderStore.ts
@@ -1,10 +1,18 @@
+import type {IItemBuilder} from "~/interfaces/interfaces";
+
 export const useCounterStore = defineStore('counter', () => {
     const builderItems = reactive([{type: 'input-text', name: 'valor'}])
     const sideMenuType = ref<'default' | 'builder'>('default')
+    const currentDragItem = ref<null | { type: string, name: string }>(null)
 
 
     function changeSideMenuType(newName: 'default' | 'builder') {
         sideMenuType.value = newName;
+    }
+
+    function changeCurrentDragItem(item: IItemBuilder) {
+        currentDragItem.value = item;
+
     }
 
     return {builderItems, sideMenuType, changeSideMenuType}
