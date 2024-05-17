eb9981da29721cebe20af79cf1bdcc34e5a57d04
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-17 18:58:58 -0400

    Add active state to ToolPanel buttons
    Additional functionality was added to the ToolPanel buttons in the Vue.js component. This change allows for an active state to be reflected for each button ('save', 'code', 'edit', and 'preview'). It improves the user experience by visually highlighting the currently active tool.

--- components/builder-components/tools-panel/ToolPanel.vue
+++ components/builder-components/tools-panel/ToolPanel.vue
@@ -1,6 +1,12 @@
 <script lang="ts" setup>
-
+import {ref} from 'vue';
 import {DsIcon} from "~/components/DesignSystem";
+
+const activeButton = ref('');
+
+const setActiveButton = (buttonName: string) => {
+    activeButton.value = buttonName;
+};
 </script>
 
 <template>
@@ -13,26 +19,29 @@ import {DsIcon} from "~/components/DesignSystem";
 
         <div class="flex space-x-1">
             <button
-                class="p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                :class="['p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out', {'bg-blue-500 text-white':activeButton === 'save'}]"
+                @click="setActiveButton('save')"
             >
                 <DsIcon name="save" title="Guardar"/>
             </button>
             <button
-                class="p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                :class="['p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out', {'bg-blue-500 text-white':activeButton === 'code'}]"
+                @click="setActiveButton('code')"
             >
                 <DsIcon name="code" title="Ver código"/>
             </button>
             <button
-                class="p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                :class="['p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out',{'bg-blue-500 text-white':activeButton === 'edit'}]"
+                @click="setActiveButton('edit')"
             >
                 <DsIcon name="edit" title="Modo edición"/>
             </button>
             <button
-                class="p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                :class="['p-1 rounded-md hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out',{'bg-blue-500 text-white':activeButton === 'preview'}]"
+                @click="setActiveButton('preview')"
             >
                 <DsIcon name="eye" title="Vista previa"/>
             </button>
         </div>
     </div>
 </template>
-
