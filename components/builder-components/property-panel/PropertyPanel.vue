b5242b3dbd8c9d2595bce6bf83b2dc2f363edbe9
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-17 02:23:10 -0400

    Refactor PropertyPanel.vue to simplify
    Improved the code structure of PropertyPanel.vue by removing redundant imports and code blocks. Removed components object relating to DsInput, DsTextArea, DsButton and Ds

--- components/builder-components/property-panel/PropertyPanel.vue
+++ components/builder-components/property-panel/PropertyPanel.vue
@@ -1,46 +1,23 @@
 <script lang="ts" setup>
-import {DsButton, DsInput, DsSelect, DsTextArea} from "~/components/DesignSystem";
 import {useCounterStore} from "~/stores/builderStore";
-import {computed} from "vue";
+
 import InputPanel from "~/components/builder-components/property-panel/components/InputPanel.vue";
 import SelectPanel from "~/components/builder-components/property-panel/components/SelectPanel.vue";
 import TextAreaPanel from "~/components/builder-components/property-panel/components/TextAreaPanel.vue";
 
 const store = useCounterStore()
-const {currentEditItem, updateItemInForm, builderItems} = toRefs(store)
+const {currentEditItem, builderItems} = toRefs(store)
 
-const components: { [key: string]: any } = {
-    DsInput,
-    DsTextArea,
-    DsButton,
-    DsSelect
-}
 
 const emit = defineEmits(["input", "update:modelValue"])
 const componentKey = ref(0);
-//
-// function handlePropertyInput(event: Event, key: number) {
-//     const target = event.target as HTMLInputElement;
-//     console.log(target.value)
-//
-//     if (currentEditItem.value) {
-//         if (!currentEditItem.value.props) {
-//             currentEditItem.value.props = {};
-//         }
-//         currentEditItem.value.props[key] = target.value;
-//     }
-//     updateItemInForm.value(currentEditItem.value!)
-// }
+
 watch(currentEditItem, () => {
     componentKey.value++;
 }, {deep: true});
 </script>
 
 <template>
-    <!--    <div v-for="(value, key) in currentEditItem.props">-->
-    <!--        <component :is="components['DsInput']"-->
-    <!--                   :label="key" :value="value" @input="(event:Event) => handlePropertyInput(event, key)"/>-->
-    <!--    </div>-->
     <InputPanel v-if="currentEditItem.name=='DsInput'" :key="componentKey"/>
     <SelectPanel v-if="currentEditItem.name=='DsSelect'" :key="componentKey"/>
     <TextAreaPanel v-if="currentEditItem.name=='DsTextArea'" :key="componentKey"/>
