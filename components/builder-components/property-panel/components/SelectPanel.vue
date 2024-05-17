536ef755f78f06dec2244887b9c9728d1279bf0c
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-17 02:41:49 -0400

    Integrate SelectPanel with CounterStore
    Modified the SelectPanel component to use the CounterStore for state management. Any changes to the input field value will now update the corresponding item in the CounterStore. Also, the label was updated from "Place Holder" to "Label".

--- components/builder-components/property-panel/components/SelectPanel.vue
+++ components/builder-components/property-panel/components/SelectPanel.vue
@@ -1,9 +1,22 @@
 <script lang="ts" setup>
 
 import {DsInput} from "~/components/DesignSystem";
+import {useCounterStore} from "~/stores/builderStore";
+
+const store = useCounterStore()
+const {currentEditItem, updateItemInForm} = toRefs(store)
+const labelValue = ref(currentEditItem.value.props.label)
+
+const emit = defineEmits(["input", "update:modelValue"])
+
+function handleInput(key: string) {
+    updateItemInForm.value(currentEditItem.value.id, key, labelValue)
+
+}
+
 </script>
 
 <template>
-    <DsInput label="Place Holder"/>
+    <DsInput v-model="labelValue" label="Label" @input="handleInput('label')"/>
 </template>
 
