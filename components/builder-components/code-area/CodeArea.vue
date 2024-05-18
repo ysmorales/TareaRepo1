ab6fcc9dd7e3d138765a605301350821d5ea9258
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-17 20:00:37 -0400

    Update CodeArea.vue styling and functionality
    The design and interactivity of CodeArea.vue component have been enhanced. This change included a switch from a standard HTML button to a Design System Button and a restyling of the component for better visual alignment and usability. The 'code' ref value for testing has also been updated.

--- components/builder-components/code-area/CodeArea.vue
+++ components/builder-components/code-area/CodeArea.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="p-2 border rounded">
-        <pre class="overflow-auto">{{ code }}</pre>
-        <button class="mt-2 px-4 py-2 bg-blue-500 text-white rounded" @click="copyCode">Copiar</button>
+    <div class="flex flex-col justify-between p-4 bg-gray-800 text-white rounded-md shadow h-full">
+        <pre class="text-sm overflow-auto text-white" v-text="code"></pre>
+        <div class="flex justify-end">
+            <DsButton @click="copyCode">Copiar
+            </DsButton>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import {ref} from 'vue';
+import {DsButton} from "~/components/DesignSystem";
 
-const code = ref('Aquí va tu código de ejemplo');
+const code = ref('<h1>Holaaaa</h1>');
 
 const copyCode = () => {
     navigator.clipboard.writeText(code.value)
@@ -28,6 +32,6 @@ pre {
     white-space: -pre-wrap; /* Opera 4-6 */
     white-space: -o-pre-wrap; /* Opera 7 */
     word-wrap: break-word; /* Internet Explorer 5.5+ */
+    max-height: 200px;
 }
 </style>
-
