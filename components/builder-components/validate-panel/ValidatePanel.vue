6d784c2053893df9e87bf1fdd21e8ac51d6bb866
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-22 18:45:11 -0400

    Updated ValidatePanel layout and UI elements
    Added additional functionality and a new layout to the ValidatePanel.vue component. The panel now includes several checkboxes allowing for different validation options. Enhanced the visual structure with a new design and added a larger, more visually intuitive layout.

--- components/builder-components/validate-panel/ValidatePanel.vue
+++ components/builder-components/validate-panel/ValidatePanel.vue
@@ -4,14 +4,48 @@ import {toRefs} from "vue";
 
 const store = useCounterStore()
 const {currentEditItem} = toRefs(store)
+
 </script>
 
 <template>
-    <div class="flex justify-between mt-5">
-        Nombre:
-        <span class="text-blue-500">{{
-                (currentEditItem?.name ?? '') + ' ' + (currentEditItem?.id ?? '')
-            }}</span>
+    <div class="p-4 bg-white shadow rounded-md">
+        <div class="flex justify-between">
+            Nombre:
+            <span class="text-blue-500">{{
+                    (currentEditItem?.name ?? '') + ' ' + (currentEditItem?.id ?? '')
+                }}</span>
+
+        </div>
+        <div class="mt-5">
+            <div class="flex items-center justify-between">
+                <label class="text-gray-700 font-medium" for="required">Requerido</label>
+                <input id="required" class="form-checkbox h-5 w-5 text-blue-500" type="checkbox">
+            </div>
+        </div>
+        <div class="mt-2">
+            <div class="flex items-center justify-between">
+                <label class="text-gray-700 font-medium" for="required">RUN</label>
+                <input id="required" class="form-checkbox h-5 w-5 text-blue-500" type="checkbox">
+            </div>
+        </div>
+        <div class="mt-2">
+            <div class="flex items-center justify-between">
+                <label class="text-gray-700 font-medium" for="required">Email</label>
+                <input id="required" class="form-checkbox h-5 w-5 text-blue-500" type="checkbox">
+            </div>
+        </div>
+        <div class="mt-2">
+            <div class="flex items-center justify-between">
+                <label class="text-gray-700 font-medium" for="required">Solo texto</label>
+                <input id="required" class="form-checkbox h-5 w-5 text-blue-500" type="checkbox">
+            </div>
+        </div>
+        <div class="mt-2">
+            <div class="flex items-center justify-between">
+                <label class="text-gray-700 font-medium" for="required">Solo números</label>
+                <input id="required" class="form-checkbox h-5 w-5 text-blue-500" type="checkbox">
+            </div>
+        </div>
     </div>
 </template>
 
