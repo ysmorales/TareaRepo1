cf69bf61826b3638996f091ca247ee9b3d208067
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-21 20:18:19 -0400

    Add color selection to ConfirmationButtonPanel
    Imported the DsSelect component to the ConfirmationButtonPanel and implemented functions to handle color change for the "cancel" and "ok" buttons. Also created a new colorData file to hold available color options. The builderItems debug code in DraggableArea has been commented out for cleaner output.

--- components/builder-components/property-panel/components/ConfirmationButtonPanel.vue
+++ components/builder-components/property-panel/components/ConfirmationButtonPanel.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 
-import {DsInput} from "~/components/DesignSystem";
+import {DsInput, DsSelect} from "~/components/DesignSystem";
 import {useCounterStore} from "~/stores/builderStore";
+import {colorData} from "~/components/builder-components/property-panel/components/data/data";
 
 const store = useCounterStore()
 const {currentEditItem, updateItemInForm} = toRefs(store)
 
 const textValueCancel = ref(currentEditItem.value.props.textCancel)
 const textValueOK = ref(currentEditItem.value.props.textOk)
+const colorValueCancel = ref(currentEditItem.value.props.colorCancel)
+const colorValueOk = ref(currentEditItem.value.props.colorOk)
 
 function handleInputTextCancel(key: string) {
     updateItemInForm.value(currentEditItem.value.id, key, textValueCancel)
@@ -16,10 +19,27 @@ function handleInputTextCancel(key: string) {
 function handleInputTextOK(key: string) {
     updateItemInForm.value(currentEditItem.value.id, key, textValueOK)
 }
+
+function handleChangeColorCancel(key: string) {
+    updateItemInForm.value(currentEditItem.value.id, key, colorValueCancel)
+}
+
+function handleChangeColorOk(key: string) {
+    updateItemInForm.value(currentEditItem.value.id, key, colorValueOk)
+}
+
+
 </script>
 
 <template>
     <DsInput v-model="textValueCancel" label="Button cancel text" @input="handleInputTextCancel('textCancel')"/>
     <DsInput v-model="textValueOK " label="Button ok text" @input="handleInputTextOK('textOk')"/>
+    <DsSelect
+        v-model="colorValueCancel"
+        :option="colorData"
+        label="Color button cancel"
+        @select="handleChangeColorCancel('colorCancel')"/>
+    <DsSelect v-model="colorValueOk" :option="colorData" label="Color button ok"
+              @select="handleChangeColorOk('colorOk')"/>
 </template>
 
---
+++ components/builder-components/property-panel/components/data/data.ts
@@ -0,0 +1,4 @@
+export const colorData = [{value: 'primary', text: 'Primary'}, {value: 'danger', text: 'Danger'}, {
+    value: 'success',
+    text: 'Success'
+}, {value: 'secondary', text: 'Secondary'}, {value: 'tertiary', text: 'Tertiary'}, {value: 'simple', text: 'Simple'}]
