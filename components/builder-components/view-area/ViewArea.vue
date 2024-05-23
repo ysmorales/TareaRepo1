be5db9b024e3ab971fd4e9d1d2f45631d3ebdb87
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-23 18:00:23 -0400

    Add required validator to view area forms
    A required validator has been added to the field validation rules in the ViewArea.vue component. The code for obtaining form values has been refactored to a useEffect function for initialization upon component mount and a watcher for updates when new items are added. Consequently, form field validation has been strengthened and more dynamic.

--- components/builder-components/view-area/ViewArea.vue
+++ components/builder-components/view-area/ViewArea.vue
@@ -11,6 +11,7 @@ import {
     DsTypography
 } from "~/components/DesignSystem";
 import {useVuelidate} from "@vuelidate/core";
+import {required} from "@vuelidate/validators";
 
 const store = useCounterStore()
 const {builderItems} = toRefs(store)
@@ -23,23 +24,54 @@ const components: { [key: string]: any } = {
     DsConfirmationButton
 }
 
-const obtainFormValues = computed(() => {
+// const obtainFormValues = computed(() => {
+//     return builderItems.value.reduce((acc, item) => {
+//         if (item && typeof item.name === 'string' && typeof item.id === 'number') {
+//             acc[item.name + item.id] = '';
+//         }
+//         return acc;
+//     }, {} as Record<string, string>);
+// });
+
+const obtainFormRules = computed(() => {
     return builderItems.value.reduce((acc, item) => {
         if (item && typeof item.name === 'string' && typeof item.id === 'number') {
-            acc[item.name + item.id] = '';
+            acc[item.name + item.id] = {required};
         }
         return acc;
-    }, {} as Record<string, string>);
+    }, {} as Record<string, any>);
 });
 
-const formValues: { [key: string]: any } = reactive(obtainFormValues);
-const formRules: { [key: string]: any } = reactive({});
+const formValues: { [key: string]: any } = reactive({});
+
+onMounted(() => {
+    builderItems.value.forEach((item) => {
+        if (item && typeof item.name === 'string' && typeof item.id === 'number') {
+            formValues[item.name + item.id] = '';
+        }
+    });
+});
+
+watch(builderItems, (newVal) => {
+    newVal.forEach((item) => {
+        if (item && typeof item.name === 'string' && typeof item.id === 'number') {
+            const key = item.name + item.id;
+            if (!(key in formValues)) {
+                formValues[key] = '';
+            }
+        }
+    });
+}, {deep: true});
+const formRules: { [key: string]: any } = reactive(obtainFormRules);
 const $V = useVuelidate(formRules, formValues);
 
 function submitForm() {
     $V.value.$touch();
+    console.log(formValues)
+    console.log(formRules)
     if (!$V.value.$error) {
         console.log(formValues)
+        console.log(formRules)
     } else {
         console.log('error')
     }
