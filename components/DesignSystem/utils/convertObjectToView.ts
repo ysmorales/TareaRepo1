b4129ef479f997cb4ee62bf2011af0fdf1ce1b9f
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-22 22:42:17 -0400

    Update convertObjectToView function with Vue3 composition API
    The convertObjectToView function has been updated to leverage the Vue3 composition API and Vuelidate2 library for form validation. The changes include creating a reactive object for form data, initializing Vuelidate, setting up validation rules, and integrating error feedback for each input field.

--- components/DesignSystem/utils/convertObjectToView.ts
+++ components/DesignSystem/utils/convertObjectToView.ts
@@ -9,18 +9,41 @@ export function objectToVueCode(formItems: FormItem[]) {
         return "No se ha editado el formulario..."
     }
 
-    let vueCode = `<script setup lang="ts">\n\n</script>\n`;
+    let imports = [
+        `import { reactive } from 'vue';`,
+        `import { useVuelidate } from '@vuelidate/core';`,
+        `import { required, minLength } from '@vuelidate/validators';`
+    ];
+
+    let vueCode = `<script setup lang="ts">\n`;
+    vueCode += imports.join(' ') + '\n\n';
+
+    let reactiveObject = 'const formValues = reactive({\n';
+    formItems.forEach((item: FormItem, index: number) => {
+        reactiveObject += `${item.name}${index}: '',\n`;
+    });
+    reactiveObject += '});\n';
+    vueCode += reactiveObject;
+
+    vueCode += `\nconst v$ = useVuelidate();\n\n`;
+
+    formItems.forEach((item: FormItem, index: number) => {
+        vueCode += `$: v$.value.formValues.${item.name}${index} = { required, minLength: minLength(3) };\n`;
+    });
+
+    vueCode += `</script>\n`;
 
     vueCode += `<template>\n<form>\n`;
 
-    formItems.forEach((item: FormItem) => {
-        vueCode += `<${item.name}`;
+    formItems.forEach((item: FormItem, index: number) => {
+        vueCode += `<${item.name} v-model="formValues.${item.name}${index}"`;
         for (let prop in item.props) {
             if (item.props[prop] !== '') {
                 vueCode += ` ${prop}="${item.props[prop]}"`;
             }
         }
         vueCode += ' />\n';
+        vueCode += `<div v-if="v$.value.formValues.${item.name}${index}.$error" class="text-red-500">Please enter a valid value</div>\n`;
     });
 
     vueCode += `</form>\n</template>`;
