83da36c10c9085a83bf80df045f6a793c0308d26
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-22 22:06:15 -0400

    Uncomment JSON.stringify in DraggableArea.vue
    The commented code for JSON.stringify(builderItems) in DraggableArea.vue has been uncommented. Previously, this code was not in use but now we have decided to use it for development purposes.

--- components/builder-components/dragable-area/DraggableArea.vue
+++ components/builder-components/dragable-area/DraggableArea.vue
@@ -58,9 +58,9 @@ function handleValidate(item: any) {
 </script>
 
 <template>
-    <!--    {{-->
-    <!--        JSON.stringify(builderItems)-->
-    <!--    }}-->
+    {{
+        JSON.stringify(builderItems)
+    }}
     <div
         class="w-full flex  border border-gray-300 shadow-md rounded-md p-5  flex-col  items-center"
         @drop="drop"
