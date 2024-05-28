78979fe0cbfe5a2a081c9e0937ce4f280bbfca72
Jorge Maikel <perezguedesmaikel@gmail.com>
2024-05-27 23:19:50 -0400

    Refactor mock data formatting in version-area component
    The mock data in the 'version-area' component has been reformatted for better readability. The code has been changed to include proper indentations and line breaks within the objects of the 'version1', 'version2' and 'version3' arrays.

--- components/builder-components/version-area/mockData.ts
+++ components/builder-components/version-area/mockData.ts
@@ -1,72 +1,88 @@
-export const version1 = [{
-    name: "DsInput-17",
-    type: "DsInput",
-    props: {"label": "", "placeholder": ""},
-    id: 1,
-    validation: {"required": true, "custom": null}
-}, {
-    name: "DsTextArea-18",
-    type: "DsTextArea",
-    props: {"label": "", "placeHolder": ""},
-    id: 2,
-    validation: {"required": true, "custom": null}
-}, {
-    "name": "DsSelect-19",
-    "type": "DsSelect",
-    "props": {"label": ""},
-    "id": 3,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsConfirmationButton-20",
-    "type": "DsConfirmationButton",
-    "props": {"label": ""},
-    "id": 4,
-    "validation": {"required": true, "custom": null}
-}]
-export const version2 = [{
-    "name": "DsInput-21",
-    "type": "DsInput",
-    "props": {"label": "", "placeholder": ""},
-    "id": 1,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsInput-22",
-    "type": "DsInput",
-    "props": {"label": "", "placeholder": ""},
-    "id": 2,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsTextArea-23",
-    "type": "DsTextArea",
-    "props": {"label": "", "placeHolder": ""},
-    "id": 3,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsSelect-24",
-    "type": "DsSelect",
-    "props": {"label": ""},
-    "id": 4,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsConfirmationButton-25",
-    "type": "DsConfirmationButton",
-    "props": {"label": ""},
-    "id": 5,
-    "validation": {"required": true, "custom": null}
-}]
-export const version3 = [{
-    "name": "DsInput-26",
-    "type": "DsInput",
-    "props": {"label": "", "placeholder": ""},
-    "id": 1,
-    "validation": {"required": true, "custom": null}
-}, {
-    "name": "DsConfirmationButton-27",
-    "type": "DsConfirmationButton",
-    "props": {"label": ""},
-    "id": 2,
-    "validation": {"required": true, "custom": null}
-}]
+export const version1 = [
+    {
+        name: "DsInput-17",
+        type: "DsInput",
+        props: {label: "", placeholder: ""},
+        id: 1,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsTextArea-18",
+        type: "DsTextArea",
+        props: {label: "", placeHolder: ""},
+        id: 2,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsSelect-19",
+        type: "DsSelect",
+        props: {label: ""},
+        id: 3,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsConfirmationButton-20",
+        type: "DsConfirmationButton",
+        props: {label: ""},
+        id: 4,
+        validation: {required: true, custom: null}
+    }
+];
+
+export const version2 = [
+    {
+        name: "DsInput-21",
+        type: "DsInput",
+        props: {label: "", placeholder: ""},
+        id: 1,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsInput-22",
+        type: "DsInput",
+        props: {label: "", placeholder: ""},
+        id: 2,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsTextArea-23",
+        type: "DsTextArea",
+        props: {label: "", placeHolder: ""},
+        id: 3,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsSelect-24",
+        type: "DsSelect",
+        props: {label: ""},
+        id: 4,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsConfirmationButton-25",
+        type: "DsConfirmationButton",
+        props: {label: ""},
+        id: 5,
+        validation: {required: true, custom: null}
+    }
+];
+
+export const version3 = [
+    {
+        name: "DsInput-26",
+        type: "DsInput",
+        props: {label: "", placeholder: ""},
+        id: 1,
+        validation: {required: true, custom: null}
+    },
+    {
+        name: "DsConfirmationButton-27",
+        type: "DsConfirmationButton",
+        props: {label: ""},
+        id: 2,
+        validation: {required: true, custom: null}
+    }
+];
 
 export const versions = [
     {version: '1.0.0', description: 'Esta es la descripción de la versión 1.0.0.', date: '2024-05-11', data: version1},
