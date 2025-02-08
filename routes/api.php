ac318f24cf8313d928b9d7b88ee684c52c817afb
Jorge Maikel <perezguedesmaikel@gmail.com>
2025-02-08 12:48:34 -0500

    register successfully

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/', function () {
     return "Builder API";
 });
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
