396c6813d232dc252d8a66d4db7d7ce4b5e80cfd
Jorge Maikel <perezguedesmaikel@gmail.com>
2025-02-08 13:06:40 -0500

    register successfully

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,4 @@ Route::get('/', function () {
     return "Builder API";
 });
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->middleware('auth:sanctum');
