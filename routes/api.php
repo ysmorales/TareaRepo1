38960268bebdab974a8a3e31210da3f15e56d644
Jorge Maikel <perezguedesmaikel@gmail.com>
2025-02-12 17:59:02 -0500

    refactor routes to comment out user endpoint and add login response handling

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 Route::get('/', function () {
     return "Builder API";
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Helpers\ResponseHelper;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login',function () {
+    return ResponseHelper::returnResponse(401, 'No tienes autorización');
+})->name('login');
+//Route::post('/login', function () {
+//    return "login";
+//});
