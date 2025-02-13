5600c0cba9917296743aa66402e78e809a57dc54
Jorge Maikel <perezguedesmaikel@gmail.com>
2025-02-12 23:49:48 -0500

    add user form enhancements with API integration and select options for role and company

--- components/Pages/user-list/UserForm.vue
+++ components/Pages/user-list/UserForm.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
-
+import useApplications from '~/api-services/applications';
 import {getErrorMessage} from "~/components/DesignSystem/utils/translateErrorMessage";
 import {DsButton, DsInput, DsSelect} from "~/components/DesignSystem";
 import {email, required} from "@vuelidate/validators";
 import {useVuelidate} from "@vuelidate/core";
+import {companyOption, rolOption} from "~/components/Pages/user-list/selcectOptions";
 
 const emit = defineEmits(["cancel"]);
 
@@ -24,34 +25,38 @@ const formRules = reactive({
 const validateForm = useVuelidate(formRules, form);
 const loading = ref(false)
 const backendError = ref<string | null>(null)
+const applicationsService = useApplications();
 
-function handleSubmit() {
+async function handleSubmit() {
     validateForm.value.$touch();
     if (!validateForm.value.$invalid) {
         loading.value = true;
         backendError.value = null;
-        // try {
-        //     console.log("Sending to back");
-        //     const response = await applicationsService.procedure.createOne("/api/login", {
-        //         email: form.email,
-        //         password: form.password,
-        //     });
-        //     if (response.codigoRetorno == 200) {
-        //         await authStore.login(response.user, response.access_token, response.expires_at);
-        //         navigateTo('/');
-        //         // internalStatus.value = "success";
-        //         // $emit('cancel');
-        //     }
-        //     if(response.codigoRetorno==404){
-        //         backendError.value = "El correo electrónico no existe en el sistema";
-        //     }
-        //     if(response.codigoRetorno==401){
-        //         backendError.value = "La contraseña es incorrecta";
-        //     }
-        // } catch (e) {
-        //     backendError.value = "Error al comunicarse con el servidor.";
-        //     console.log('Viendo error en consola.',e);
-        // }
+        try {
+            const response = await applicationsService.procedure.createOne("/api/users", {
+                name: form.name,
+                email: form.email,
+                password: "password123",
+                password_confirmation: "password123",
+                role: "visual",
+                empresa: "Chile Atiende"
+            });
+            if (response.codigoRetorno == 200) {
+                // await authStore.login(response.user, response.access_token, response.expires_at);
+                navigateTo('/');
+                // internalStatus.value = "success";
+                // $emit('cancel');
+            }
+            if (response.codigoRetorno == 404) {
+                backendError.value = "El correo electrónico no existe en el sistema";
+            }
+            if (response.codigoRetorno == 401) {
+                backendError.value = "La contraseña es incorrecta";
+            }
+        } catch (e) {
+            backendError.value = "Error al comunicarse con el servidor.";
+            console.log('Viendo error en consola.', e);
+        }
 
         loading.value = false;
     }
@@ -63,9 +68,10 @@ function handleSubmit() {
         <DsInput v-model="form.name" :error="getErrorMessage(validateForm?.name.$errors[0])" label="Nombre Completo:"/>
         <DsInput v-model="form.email" :error="getErrorMessage(validateForm?.email.$errors[0])"
                  label="Correo electrónico:"/>
-        <DsSelect v-model="form.rol" :error="getErrorMessage(validateForm?.rol.$errors[0])" label="Rol:"/>
+        <DsSelect v-model="form.rol" :error="getErrorMessage(validateForm?.rol.$errors[0])" :option="rolOption"
+                  label="Rol:"/>
         <DsSelect v-model="form.empresa" :error="getErrorMessage(validateForm?.empresa.$errors[0])"
-                  label="Empresa:"/>
+                  :option="companyOption" label="Empresa:"/>
         <div class="flex justify-end mt-5 p-2">
             <DsButton class="mr-2" color="secondary" text="cancelar" type="button" @click="$emit('cancel')"/>
             <DsButton text="Guardar" type="submit"/>
