9a8112e104f7284e3bc750db6e55287e0e04753d
Jorge Maikel <perezguedesmaikel@gmail.com>
2025-02-12 21:02:29 -0500

    update UserRequest validation and refactor user routes for improved structure

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\Empresa;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Enums\Role;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRequest extends FormRequest
 {
@@ -14,7 +17,36 @@ class UserRequest extends FormRequest
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
             'role' => 'required|in:' . implode(',', Role::getValues()),
-            'empresa' => 'nullable|string|max:255',
+            'empresa'=> 'required|in:' . implode(',', Empresa::getValues()),
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.string' => 'The name must be a string.',
+            'name.max' => 'The name may not be greater than 255 characters.',
+            'email.required' => 'The email field is required.',
+            'email.string' => 'The email must be a string.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.max' => 'The email may not be greater than 255 characters.',
+            'email.unique' => 'The email has already been taken.',
+            'password.required' => 'The password field is required.',
+            'password.string' => 'The password must be a string.',
+            'password.min' => 'The password must be at least 8 characters.',
+            'password.confirmed' => 'The password confirmation does not match.',
+            'role.required' => 'The role field is required.',
+            'role.in' => 'The selected role is invalid.',
+            'empresa.required' => 'The empresa field is required.',
+            'empresa.in' => 'The selected empresa is invalid.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors()
+        ], 422));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
-//
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
+
 
 Route::get('/', function () {
     return "Builder API";
@@ -20,7 +17,12 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register'])->middleware('auth:sanctum');
 Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
-Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/users', [UserController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/users/{user}', [UserController::class, 'show'])->middleware('auth:sanctum');
+Route::put('/users/{user}', [UserController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth:sanctum');
 
 //Route::get('/test-email', function () {
 //    Mail::raw('This is a test email', function ($message) {
